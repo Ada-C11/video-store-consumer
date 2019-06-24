4715d5695312c6c3ab08dde3697a4ef43e06dc0e
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-24 16:00:32 -0700

    added Link to movies/search

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,6 +8,7 @@ class HomePage extends Component {
         <h3>HomePage</h3>
         <p>
           <Link to="/movies">Movies</Link>
+          <Link to="/movies/search">Search for a Movie</Link>
           <Link to="/customers">Customers</Link>
         </p>
       </div>
