ce98bd63bf8cc6924d5b8a77b2a8c7bbe6dac92e
MyriamWD <myriam.waldend@gmail.com>
2019-06-24 16:03:08 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
+import MovieLibrary from './components/MovieLibrary';
+import Customer from './components/Customer';
+import Checkout from './components/Checkout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/search'>Search</Link>
+                </li>
+                <li>
+                  <Link to='/movielibrary'>Movie Library</Link>
+                </li>
+                <li>
+                  <Link to='/customers'>Customers</Link>
+                </li>
+                <li>
+                  <Link to='/checkout'>Checkout</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route path="/" exact component={App} />
+            <Route path="/search" component={Search} />
+            <Route path="/movielibrary" component={MovieLibrary} />
+            <Route path="/customers" component={Customer} />
+            <Route path="/checkout" component={Checkout} />
+
+          </Router>
         </header>
-        <p>
-        </p>
+
+
+
+
+      
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+  render() {
+    return(
+      <div>
+        bgshjdvfh
+      </div>
+
+    )
+
+  }
+}
+
+export default Search;
