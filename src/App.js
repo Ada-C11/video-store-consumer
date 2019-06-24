70c11a3816c2d8e53386a8eb69f40b01bbba2c1a
Bitaman <b.amani.s@gmail.com>
2019-06-24 16:29:41 -0700

    made the Movie search component with a search form inside

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import MovieSearch from "./components/MovieSearch"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MovieSearch />
-        <MovieLibrary />
-        <CustomerList />
+        {/* <MovieLibrary /> */}
+        {/* <CustomerList /> */}
       </div>
     );
   }
