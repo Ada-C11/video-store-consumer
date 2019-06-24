ff4075f02ff9c19f0877ab34fcfa941228a0604c
Hana <hanalways@gmail.com>
2019-06-24 16:05:59 -0700

    refactor AppRouter as a component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppRouter from './components/AppRouter';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppRouter />
 
-function Index() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
-function AppRouter() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
-export default AppRouter;
+export default App;
---
+++ src/components/AppRouter.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './AppRouter.css';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Search() {
+  return <h2>Search Movies</h2>;
+}
+
+function Library() {
+  return <h2>Movie Library</h2>;
+}
+
+function Customers() {
+  return <h2>Customers</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search/">Search Movies</Link>
+            </li>
+            <li>
+              <Link to="/library/">Movie Library</Link>
+            </li>
+            <li>
+              <Link to="/customers/">Customers</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/search/" component={Search} />
+        <Route path="/library/" component={Library} />
+        <Route path="/customers/" component={Customers} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
