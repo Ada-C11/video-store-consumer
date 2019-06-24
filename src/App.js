afe24b2ed68113a7fa29cd221595bfa609274f21
Nidhi <nidhixpatel@gmail.com>
2019-06-24 15:58:50 -0700

    added components for pages library search and customer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Library from './components/Library.js';
+import Search from './components/Search.js';
+import Customers from './components/Customers.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -27,17 +30,17 @@ class App extends Component {
               <Link to="/search/">Search Movies</Link>
             </li>
            <li>
-            <Link to="/movies/">Movies</Link>
+            <Link to="/library/">Library</Link>
           </li>
           <li>
             <Link to="/customers/">Customers</Link>
           </li>
         </ul>
       </nav>
-
-      <Route path="/" exact component={Index} />
+{/* 
+      <Route path="/" exact component={Index} /> */}
       <Route path="/search/" component={Search} />
-      <Route path="/movies/" component={Movies} />
+      <Route path="/library/" component={Library} />
       <Route path="/customers/" component={Customers} />
       </div>
       </Router>
