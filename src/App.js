b279d1afff6002d45a17d7166026f7ae7eea556a
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-24 14:53:06 -0700

    framework for react router added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Header />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={Search} />
+          <Route path="/library" component={Library} />
+          <Route path="/customers" component={Customers} />
+        </div>
+      </Router>
     );
   }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+
+function Library() {
+  return <h2>Library</h2>;
+}
+
+function Customers() {
+  return <h2>Customers</h2>;
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/search">Search</Link>
+      </li>
+      <li>
+        <Link to="/library">Library</Link>
+      </li>
+      <li>
+        <Link to="/customers">Customers</Link>
+      </li>
+    </ul>
+  );
 }
 
 export default App;
