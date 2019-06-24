db92ce8a5211a9e277c232a0a2be45cde20a5277
Ariana Bray <aribray@gmail.com>
2019-06-24 14:10:11 -0700

    imported react-router-dom into App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
+  
+
+  Index = () => {
+    return <h2>Home</h2>;
+  }
+
+  About = () => {
+    return <h2>About</h2>;
+  }
+
+  Users = () => {
+    return <h2>Users</h2>;
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  return (
+    <div>
+      <Router >
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/movies/">All Movies</Link>
+              </li>
+              <li>
+                <Link to="/customers/">Customers</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} /> */}
+        </div>
+      </Router>
+    </div>
+  );
   }
 }
 
 export default App;
+
+
