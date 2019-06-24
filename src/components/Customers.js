77bf9e65ab05c92e1b26fcd06315ee527ed9f71e
Hana <hanalways@gmail.com>
2019-06-24 16:28:52 -0700

    add components

---
+++ src/components/Customers.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Customer extends Component {
+  render {
+    return()
+  }
+}
+
+export default Customer;
---
+++ src/components/Library.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Library extends Component {
+  render {
+    return()
+  }
+}
+
+export default Library;
