3581c97a04161ee1d6cb85a4877d0422bf0aff36
Nidhi <nidhixpatel@gmail.com>
2019-06-24 15:51:17 -0700

    working on react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,33 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      
+
+      <Router>
+        <div>
+          <nav>
+            <ul>
+               <li>
+               <Link to="/">Home</Link>
+              </li>
+               <li>
+              <Link to="/search/">Search Movies</Link>
+            </li>
+           <li>
+            <Link to="/movies/">Movies</Link>
+          </li>
+          <li>
+            <Link to="/customers/">Customers</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/search/" component={Search} />
+      <Route path="/movies/" component={Movies} />
+      <Route path="/customers/" component={Customers} />
+      </div>
+      </Router>
       </div>
     );
   }
