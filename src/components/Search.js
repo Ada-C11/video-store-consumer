65ec80a4456c356460b2a357d877c5ba7e7c29cf
Jansen <v.jansen.martin@gmail.com>
2019-06-24 16:52:42 -0700

    Pass hardcoded movie data from Search to SearchResults components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SearchResults from './SearchResults';
+import MOVIE_DATA from '../data/movie-data.json';
 
 class Search extends Component {
     constructor() {
         super();
 
         this.state = {
-            searchResults: [],
+            movies: MOVIE_DATA,
         }
+
     }
 
     render() {
         return (
-            <SearchResults />
+            <SearchResults movieData={this.state.movies} />
         );
     }
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types';
 
 const SearchResults = (props) => {
 
+    console.log("This SHOULD be the movie data...")
+    console.log(props.movieData);
+    const movies = props.movieData.map((movie, i) => {
+        return <li key={i}>{movie.title}</li>
+    });
+
     return (
-        <p>Hello!!!</p>
+        <div>
+            {movies}
+        </div>
     );
 }
 
 export default SearchResults
+
+SearchResults.propTypes = {
+    movieData: PropTypes.array,
+}
