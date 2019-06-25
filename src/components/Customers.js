caf316c69aae986fcc8a078a50033fedcd938bae
Jansen <v.jansen.martin@gmail.com>
2019-06-25 14:06:08 -0700

    Render customer list via API call

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -14,13 +14,41 @@ class Customers extends Component {
     }
 
     componentDidMount() {
-        axios.get();
+
+        const getURL = "http://localhost:3002/customers"
+    
+        axios.get(getURL)
+          .then((response) => {
+            console.log(response.data);
+
+            const customers = response.data.map((customer) => {
+                const customerEntry = {
+                    name: customer.name,
+                }
+
+                return customerEntry;
+            })
+
+            this.setState({ customers });
+          })
+          .catch((error) => {
+            //Do something if there's an error
+            console.log(error);
+            // this.setState({ error: `${error.message} while loading your cards!` });
+
+          });
     }
 
     render() {
+
+        const customerComponents = this.state.customers.map((customer, index) => {
+            return (
+                <li key={index}>{customer.name}</li>
+            )
+        })
         return (
             <div>
-                <p>Inside Customer List</p>
+                { customerComponents }
             </div>
         );
     }
