926987d42726d5b284e49743f0c6e946c2fcfc4e
Bitaman <b.amani.s@gmail.com>
2019-06-25 15:20:59 -0700

    made the axios call inside the form

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -5,83 +5,63 @@ import axios from 'axios';
 import MovieSearchForm from "./MovieSearchForm"
 
 class MovieSearch extends Component {
-    constructor() {
-      super();
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      // error: ""
+    };
+  }
+
+  // componentDidMount() {
+
+  getresult = (movies) => {
+    const collection = movies.map((movie)=>{
+      this.state.movies.push(movie)
+    })
+  }
+     
+   
+      // this.setState({
+      //   movies: movie
+      // })
+
+    
   
-      this.state = {
-        Movies: [],
-        error: ""
-      };
-    }
     
-    // componentDidMount() {
-    //     const URL = "https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false"
-    //     axios.get(URL)
-    //       .then((response) => {
-    //         const movies = response.data.map((movie) => {
-    //           const newMovie = {
-    //             ...movie,
-    //           }
-    //           return newMovie
-    //         })
-    //         console.log(movies)
-    //         this.setState({
-    //           movies: movies,
-    //         });
-    //       })
-    //       .catch((error) => {
-    //         console.log(error);
-    //         alert('Error happened');
-    //         this.setState({ error: error.message });
-    //       });
-    //   }
-    // searchMovieCallback = (movie) => {
-    //     const movieIds = this.state.movies.map(item => item.movie.id)
-    
-    //     this.setState({
-    //       movies: [...this.state.movies, { movie:{...movie, id: Math.max(...movieIds) + 1 }}]
-    //     });
-    // //     axios.post(`${this.props.url}${this.props.boardName}/cards`, {text: card.text, emoji: card.emoji})
-    // //     .then((response) => {
-    // //       console.log(response)
-    // //     })
-    // //     .catch((error)=> {
-    // //       console.log(error)
-    // //       alert('Error happened');
-    // //       this.setState({ error });
-    // //     })
-    //   }
+  
+
 
     render() {
-    //     const moviecollection = this.state.movies.map((movie, i) => {
-    //       return ([
-    //         <Movie
-    //           key={i}
-    //           id={re["card"].id}
-    //           text={card["card"].text}
-    //           emoji={card["card"].emoji}
-    //           onDeleteCard={this.onDeleteCard}
-    //         />]
-    //       )
-    //     })
-        return (
-    //       <div>
-    //         <header classname="validation-errors-display">
-    //           {this.state.error}
-    //         </header>
-    <div>
-            <div classname="board">
-            </div>
-                    <div>
-                    <MovieSearchForm searchMovieCallback={this.searchMovieCallback}/>
-                  </div>
-                  </div>
-    //         <div>
-    //           <NewCardForm addCardCallback={this.addCardCallback}/>
-    //         </div>
-    //       </div>
-        )
-      };
-    };
+      console.log(this.state.movies)
+        const moviecollection = this.state.movies.map((movie) => {
+            return(
+            <section>
+            <span> {movie["title"]} </span>
 
-export default MovieSearch;
+            <button
+              type="button"
+              className="btn btn-danger"
+              aria-label="Close"
+            >
+              ADD
+            </button>
+          </section>
+          )
+        })
+      return (
+        <div>
+
+        <div>
+          <MovieSearchForm   getresultcallback={this.getresult}/>
+          {/* getresultcallback={this.getresult} */}
+        </div>
+        <div>{this.state.movies}</div>
+        </div>
+
+      )
+    };
+  };
+
+  export default MovieSearch;
--- src/components/MovieSearchForm.js
+++ src/components/MovieSearchForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import './MovieSearchForm.css';
 
@@ -9,19 +10,43 @@ class MovieSearchForm extends Component {
 
         this.cleared = {
             text: "",
+            // movies: [],
+            hasmovies: false
         };
 
         this.state = { ...this.cleared }
     }
     searchMovie = (event) => {
         event.preventDefault();
+            const URL = `https://api.themoviedb.org/3/search/movie?api_key=bb031365ec3e7cc087920804b23e74b1&language=en-US&page=1&query=${this.state.text}&include_adult=false`
+            axios.get(URL)
+              .then((response) => {
+                  console.log(response)
+                  const movies = response.data["results"]
+                // const movies = response.data["results"].map((movie) => {
+                //   const newMovie = {
+                //     title: movie["title"]
+                //   }
+                //   return newMovie
+                // })
+                console.log(response.data["results"])
+                // this.setState({
+                //   movies: movies,
+                //   hasmovies: true
+                // });
+                this.props.getresultcallback(movies)
+
+                // console.log(this.state.movies)
+              })
+              .catch((error) => {
+                console.log(error);
+                alert('Error happened');
+                this.setState({ error: error.message });
+              });
 
         const movie = this.state;
         console.log(this.props)
-
-        this.props.searchMovieCallback(movie)
-
-        this.setState({ ...this.cleared });
+        this.setState({ movies: movie });
     }
     onInputChange = (event) => {
         const updatedState = {};
@@ -33,6 +58,21 @@ class MovieSearchForm extends Component {
         this.setState(updatedState);
     }
     render() {
+        // const moviecollection = this.state.movies.map((movie) => {
+        //     return(
+        //     <section>
+        //     <span> {movie} </span>
+
+        //     <button
+        //       type="button"
+        //       className="btn btn-danger"
+        //       aria-label="Close"
+        //     >
+        //       ADD
+        //     </button>
+        //   </section>
+        //   )
+        // })
         return (
             <div className="new-card-form">
             <form
