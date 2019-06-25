9857cd5e4b24618cb3f006568abf1b367a01a9bb
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-25 11:16:19 -0700

    added onChange event handling for text input field

--- src/components/MovieSearchBar.js
+++ src/components/MovieSearchBar.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
 
 class MovieSearchBar extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      query: "",
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target);
+    const updatedState = this.state;
+
+    const field = event.target.name;
+    const value = event.target.value;
+
+    updatedState[field] = value;
+    this.setState(updatedState);
+  };
+
+ 
+
   render() {
+
+  
     return (
       <div>
         <h5>MoviesSearchBar</h5>
         <form>
-          <input type="text" name="query" value="" defaultValue="Type in your search terms" />
+          <input type="text" name="query" onChange={this.onInputChange} value={this.state.query} defaultValue="Type in your search terms" />
           <button type="submit">Search</button>
         </form>
       </div>
