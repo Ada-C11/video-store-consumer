21e7c4e8b3d4d9a7378546ed41c23a5f8acefebf
Grace Shea <grace.m.shea@gmail.com>
2019-06-25 15:26:33 -0700

    addMovietoLibrary code added to MovieSearch.js

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -46,10 +46,61 @@ class MovieSearch extends React.Component {
       console.log(error);
       this.setState({error: error.message})
     });
- }
+  }
 
-  // TO DO: event handlers
-  //  TO DO: add movie to library with axios post
+
+  addMovieToLibrary = (movieId) => {
+    // TO DO: make sure following line syntax is okay
+    const clickedMovie = this.state.movies.find(movie)
+
+    const movieData = {
+      title: clickedMovie.title,
+      overview: clickedMovie.overview,
+      release_date: clickedMovie.release_date,
+      inventory: clickedMovie.inventory,
+      image_url: clickedMovie.image_url,
+      external_id: clickedMovie.external_id
+    }
+
+    axios.post('http://localhost:3000/movies/', movieData)
+     .then((response) => {
+       alert(`Sucessfully added ${movieData.title} to rental library!`);
+     })
+     .catch((error) => {
+       alert(`Error: ${error.message}`);
+     })
+  }
+
+ render() {
+   const searchResults = this.state.movies.map((movie, index) => {
+     return <Movie key={index}
+     id={movie.external_id}
+     title={movie.title}
+     releaseDate={movie.release_date}
+     imageUrl={movie.image_url}
+     callback={this.addMovieToLibrary}
+     buttonText='Add to Library' />
+   });
+
+   return (
+     <section>
+       <div className="search-form-container">
+         <form onSubmit={this.onFormSubmit}>
+            <input name="title" placeholder="Movie title..." type="text"
+              className= "form-control form-control-lg search-form"
+              value={this.state.title}
+              onChange={this.onInputChange}
+              />
+            <input type="submit" value="Search" className="btn btn-outline-dark btn-lg"/>
+         </form>
+       </div>
+
+       <div className="search-results-container">
+         {searchResults}
+       </div>
+     </section>
+   );
+ }
 }
 
 export default MovieSearch;
