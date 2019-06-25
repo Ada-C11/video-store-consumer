57a99740f363a5be3ba72ad2f68443d53a747336
Nara <naraa_520@yahoo.com>
2019-06-24 22:10:49 -0700

    CustomerListPage component that lists Customer component

--- src/components/CustomerListPage.js
+++ src/components/CustomerListPage.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
-
+import axios from 'axios'
+import Customer from './Customer'
+import 'bootstrap/dist/css/bootstrap.css';
 class CustomerListPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers:[],
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    const URL = "http://localhost:3001/customers"
+    axios.get(URL)
+      .then((response) => {
+        this.setState({ customers: response.data });
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+      });
+  }
   render() {
+    const { customers } = this.state;
+    const customerList = customers.map((customers) => {
+      const {id, name, registered_at, address, 
+            city, state, postal_code, phone, 
+            account_credit, movies_checked_out_count} = customers;
+      return ( <Customer key={id} id={id} name={name} registered_at={registered_at}
+              address={address} city={city} state={state} postal_code={postal_code}
+              phone={phone} account_credit={account_credit} movies_checked_out_count={movies_checked_out_count} 
+              /> )
+    });
+
+    const errorSection = (this.state.error) ? 
+    (<section className="error">
+       Error: {this.state.error}
+     </section>) : null;
+
     return (
-      <div>
-        <h3>CustomerListPage</h3>
-        <p>
-          hello customer
-        </p>
-      </div>
+      <section>
+        {errorSection}
+        <div>
+          <table class="table table-striped">
+            <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Registed At</th>
+              <th scope="col">Address</th>
+              <th scope="col">City</th>
+              <th scope="col">State</th>
+              <th scope="col">Postal Code</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Account Credit</th>
+              <th scope="col">Movies Checked Out Count</th>
+            </tr>
+            </thead>
+            <tbody>
+              {customerList}
+            </tbody>
+          </table>
+        </div>
+      </section>
     );
   }
 }
