520b6c1fa37ae61d7089aa5ca45f7b6f2cfc05f0
Riyo <riyo.perry@gmail.com>
2019-06-25 12:19:29 -0700

    started search form

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,15 +7,66 @@ import axios from 'axios';
 
 
 class Search extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
+        this.state = {
+            title: ""
+        };
+    }
 
-    };
-  }
+    onInputChange = (event) => {
+        const updatedState = {};
 
-  componentDidMount() {
+        const field = event.target.name;
+        const value = event.target.value;
 
-  }
+        updatedState[field] = value;
+        this.setState(updatedState);
+    }
+
+
+    clearForm = () => {
+        this.setState({
+            title: ""
+        });
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.searchCallback(this.state.title)
+        this.clearForm()
+    }
+
+    render() {
+        return (
+            <div className="search-form">
+                <div className="search-form__header">
+                    Write a Inspirational Card!
+                </div>
+                <form className="new-card-form__form" onSubmit={this.onFormSubmit}>
+                    <div>
+                        <textarea name="text" value={this.state.text} type="text" onChange={this.onInputChange} className="new-card-form__form-textarea" />
+                    </div>
+                    <div>
+                        <select className="new-card-form__form-select" onChange={this.onInputChange} name="emoji" value={this.state.emoji}  >
+                            {this.emojiOptions(EMOJI_LIST)}
+                        </select>
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit Card" className="new-card-form__form-button" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+
+
+
 }
+Search.propTypes = {
+    searchCallback: PropTypes.func
+  }
+  
