ec325e6dd390ff36bfc91a1541f6d270f7b8f86c
Chantal Demissie <chantaldemissie@yahoo.com>
2019-06-25 15:17:58 -0700

    movies displayed

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Library extends Component {
-  render() {
+    constructor() {
+      super();
+      this.state = {
+        library: [],
+      };
+    }
+  
+    componentDidMount() {
+      this.updateLibrary()
+    }
+
+    updateLibrary = () => {
+      const url = `http://localhost:3000/movies`
+      axios.get(url)
+        .then((response) => {
+          this.setState({ library: response.data });
+          console.log(response)
+        }) 
+        .catch((error) => {
+          this.setState({ error: error.message });
+        });
+    }
+
+    render() {
+      const allmovies = this.state.library.map((movie, i) => {
+         return <li>{movie.title}</li>
+      })
+
     return (
       <div className="App">
         <header className="App__header">
-          <h1 className="App__title">Exquisite Corpse</h1>
+          <h1 className="App__title">Movies</h1>
           <p className="App__intro-text">
-            Exquisite corpse, also known as exquisite cadaver (from the original French term cadavre exquis), is a method by which a collection of words or images is collectively assembled. Each collaborator adds to a composition in sequence, either by following a rule (e.g. "The adjective noun adverb verb the adjective noun." as in "The green duck sweetly sang the dreadful dirge.") or by being allowed to see only the end of what the previous person contributed.
+           {allmovies}
           </p>
         </header>
       </div>
