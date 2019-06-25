82e526e950750ca3298e6921d9440d25f2cd238a
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-06-25 12:06:41 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Library from './components/Library';
 import Customer from './components/Customer';
+import './App.css'
+import { throwStatement } from "@babel/types";
 
 
 function Index() {
@@ -16,6 +18,7 @@ class AppRouter extends Component {
     this.state = {
       currentMovie: undefined,
       currentCustomer: undefined,
+      selectedPage: "/"
 
     }
   }
@@ -38,30 +41,38 @@ class AppRouter extends Component {
     });
   }
 
-  render() {
+  setSelectedPage = (event) => {
+    this.setState ({
+      selectedPage: window.location.pathname,
+    });
+    console.log(window.location.pathname);
+  }
 
+  render() {
     return (
       <Router>
-        <div>
+        <div className="full-wrapper">
           <nav>
             <ul>
-              <li>
-                <Link to="/">Home</Link>
+              <li className={this.state.selectedPage === "/" ? "selected" : ""}>
+                <span onClick={this.setSelectedPage}><Link to="/">Home</Link></span>
               </li>
-              <li>
-                <Link to="/library/">Library</Link>
+              <li className={this.state.selectedPage === "/library/" ? "selected" : ""}>
+              <span onClick={this.setSelectedPage}><Link to="/library/">Library</Link></span>
               </li>
-              <li>
-                <Link to="/customers/">Customers</Link>
+              <li className={this.state.selectedPage === "/customers/" ? "selected" : ""}>
+              <span onClick={this.setSelectedPage}><Link to="/customers/">Customers</Link></span>
               </li>
             </ul>
           </nav>
-
-          <div>{this.state.currentMovie ? `Selected Movie: ${this.state.currentMovie.title}` : ""}</div>
-          <div>{this.state.currentCustomer ? `Selected Customer: ${this.state.currentCustomer.name}` : ""}</div>
-          <Route path="/" exact component={Index} />
-          <Route path="/library/" render={(props) => <Library {...props} onSelectMovieCallback={this.onSelectMovie} />} />
-          <Route path="/customers/" render={(props) => <Customer {...props} onSelectCustomerCallback={this.onSelectCustomer} />} />
+          <main>
+            <div className="selected-movie">{this.state.currentMovie ? `Selected Movie: ${this.state.currentMovie.title}` : ""}</div>
+            <div className="selected-customer">{this.state.currentCustomer ? `Selected Customer: ${this.state.currentCustomer.name}` : ""}</div>
+            <div className={this.state.currentCustomer && this.state.currentMovie ? "checkout-button" : "hidden"}><span>Check Movie Out</span></div>
+            <Route path="/" exact component={Index} />
+            <Route path="/library/" render={(props) => <Library {...props} onSelectMovieCallback={this.onSelectMovie} />} />
+            <Route path="/customers/" render={(props) => <Customer {...props} onSelectCustomerCallback={this.onSelectCustomer} />} />
+          </main>
         </div>
       </Router>
 
