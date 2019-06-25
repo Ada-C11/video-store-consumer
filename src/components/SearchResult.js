0d425044ee297f4219b5d532b8ec420e088fdcec
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-25 16:41:56 -0700

    flushed out component

---
+++ src/components/SearchResult.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const SearchResult = (props) => {
+  const {id, title, overview, release_date, external_id, 
+        image_url, onSelectHandler} = props;
+  
+  const onMovieClick = () => {
+    onSelectHandler(props); 
+  }
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{overview}</td>
+      <td>{release_date}</td>
+      <td>
+        <button className="btn btn-primary" onClick={onMovieClick} >
+          Add to Library
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+
+export default SearchResult;
