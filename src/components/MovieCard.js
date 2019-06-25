8adfb3627098fec5cdfcd561d0ab3f1e7543c761
Erica Norris <norrise120@gmail.com>
2019-06-25 13:07:13 -0700

    updated moviecard to actually include real stuff:

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './MovieCard.css';
 
 
 
 const MovieCard = (props) => {
-  const { id, title, overview, release_date, image_url, external_id, inventory } = props;
+  const { title, overview, release_date, image_url, external_id, inventory } = props;
 
   return (
-    <div>{title}</div>
+    <div class="card" style={{width: 18 + 'rem'}}>
+      <img class="card-img-top" src={image_url} alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{overview}</p>
+        <p class="card-text">{release_date}</p>
+        <p class="card-text">Inventory: {inventory}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   );
 };
 
