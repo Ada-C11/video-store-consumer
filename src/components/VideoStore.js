1fcc8c29ec1995b7f8498692bb9258b47be9aa1c
Amy Phung <phung.amy85@gmail.com>
2019-06-24 17:00:46 -0700

    shows a list of customers

--- src/components/VideoStore.js
+++ src/components/VideoStore.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MovieLibrary from './MovieLibrary';
+import Customer from './Customer';
+import axios from 'axios';
 
 
 class VideoStore extends Component {
@@ -11,13 +13,34 @@ class VideoStore extends Component {
             currentMovie: "",
             currentCustomer: 0,
             customers: [],
-            movies: [],
-
         };
     }
 
     componentDidMount() {
+        const fullUrl = "http://localhost:3000/customers"
+        axios.get(fullUrl)
+            .then((response) => {
+                const customers = response.data.map((customer) => {
+                    const { id, name, city, address, postal_code, phone, account_credit, movies_checked_out_count } = customer;
+                    const newCustomer = {
+                        id: id,
+                        name: name,
+                        city: city,
+                        address: address,
+                        postal_code: postal_code,
+                        phone: phone,
+                        account_credit: account_credit,
+                        movies_checked_out_count: movies_checked_out_count,
+                    }
+                    return newCustomer;
+                })
 
+                this.setState({ customers });
+
+            })
+            .catch((error) => {
+                this.setState({ errorMessages: error.message });
+            });
     }
 
     currentCustomerCallback = (id) => {
@@ -33,10 +56,17 @@ class VideoStore extends Component {
     }
 
     render() {
+        const displayCustomers = this.state.customers.map((customer) => {
+            const { id, name } = customer;
+            return (<Customer id={id} name={name} key={id} />)
+        })
         return (
-            <section className="movielibrary-button">
-                <MovieLibrary
 
+            <section className="movielibrary-button">
+                <section className="customer_list">
+                    {displayCustomers}
+                </section>
+                <MovieLibrary
                     currentMovieCallback={this.currentMovieCallback}
                     currentCustomerCallback={this.currentCustomerCallback}
                 />
