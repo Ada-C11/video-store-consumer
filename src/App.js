d46f3e0e24f65b026d80b2ef1a670497aa696687
Shubha Rajan <shubharajan94@gmail.com>
2019-06-25 15:56:11 -0700

    bootstrap nav, and app is now a class that tracks selected customer and movie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { LinkContainer } from 'react-router-bootstrap'
 
@@ -11,51 +11,73 @@ import { Nav } from 'react-bootstrap'
 
 //  Below code is modified from React Router Tutorial https://reacttraining.com/react-router/web/guides/quick-start
 
-function Index() {
-  return <h2>Home</h2>;
-}
 
-function Search() {
-  return <h2>Search</h2>;
-}
+class AppRouter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedMovie: undefined,
+      selectedCustomer:undefined,
+    }
+  }
 
+  selectCustomer = (customer) => {
+    console.log(customer);
+    this.setState({
+        selectedCustomer: customer,
+    });
+    
+  }
+  selectMovie = (movie) => {
+    console.log(movie);
+    this.setState({selectedMovie: movie});
+  }
 
-function AppRouter() {
-  return (
-    <Router>
-      <div>
-      <Nav variant="tabs" as="ul"> 
-        <Nav.Item as="li">
-          <LinkContainer to="/home">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-              
-        </Nav.Item>
-        <Nav.Item as="li" >
-          <LinkContainer to="/search">
-              <Nav.Link>Search</Nav.Link>  
-          </LinkContainer>
-        </Nav.Item>
-        <Nav.Item as="li" eventKey="/library">
-          <LinkContainer to="/library">
-                <Nav.Link>Library</Nav.Link>  
-          </LinkContainer>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <LinkContainer to="/customers">
-                <Nav.Link>Customers</Nav.Link>  
-          </LinkContainer>
-        </Nav.Item>
-
-      </Nav>
-
-        <Route path="/" exact component={Index} />
-        <Route path="/search/" exact component={Search} />
-        <Route path="/library/" component={MovieLibrary} />
-        <Route path="/customers/" component={CustomerList} />
-      </div>
-    </Router>
-  );
+  render(){
+    return (
+      <Router>
+        <div>
+        <Nav variant="tabs" as="ul"> 
+          <Nav.Item as="li">
+            <LinkContainer to="/home">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+                
+          </Nav.Item>
+          <Nav.Item as="li" >
+            <LinkContainer to="/search">
+                <Nav.Link>Search</Nav.Link>  
+            </LinkContainer>
+          </Nav.Item>
+          <Nav.Item as="li" eventKey="/library">
+            <LinkContainer to="/library">
+                  <Nav.Link>Library</Nav.Link>  
+            </LinkContainer>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <LinkContainer to="/customers">
+                  <Nav.Link>Customers</Nav.Link>  
+            </LinkContainer>
+          </Nav.Item>
+        </Nav>
+  
+          <Route path="/" exact component={Index} />
+          <Route path="/search/" exact component={Search} />
+          
+           />
+          <Route
+              path='/library'
+              render={(props) => <MovieLibrary {...props} selectMovie={this.selectMovie} />}
+          />
+          <Route
+              path='/customers'
+              render={(props) => <CustomerList {...props} selectCustomer={this.selectCustomer} />}
+          />
+        </div>
+      </Router>
+    );
+  }
+  
 }
 
 export default AppRouter;
