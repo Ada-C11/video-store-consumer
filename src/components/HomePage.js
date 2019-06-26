9ca9b1c8bdb87c360da0be6d8c52334228fd81f3
Nara <naraa_520@yahoo.com>
2019-06-26 13:31:57 -0700

    Homepage CSS to add padding

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import './HomePage.css';
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h3>HomePage</h3>
+      <div className="homepage">
         <div><Link to="/library">Movie Rental Library</Link></div> 
         <div><Link to="/search">Search for a Movie</Link></div>
         <div><Link to="/customers">Customers</Link></div>
