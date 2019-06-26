61d6556c283e9f11b2cd45dbbca922120d1cdfce
Shubha Rajan <shubharajan94@gmail.com>
2019-06-26 09:37:36 -0700

    error message toast when attempting to rent movie

--- src/components/Selected.js
+++ src/components/Selected.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Card, Button} from 'react-bootstrap'
+import {Card, Button, Toast} from 'react-bootstrap'
+import axios from 'axios';
 
 import './Selected.css'
 class Selected extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showToast: false,
+            toastTitle: '',
+            toastMessage: '',
+        }
+    }
     onReserveButtonClick = () =>{
-        console.log('reserve me!');
+        if (this.props.movie && this.props.customer) {
+            const endpoint = `http://localhost:3000/movies/${this.props.movie.title}/check-out`;
+           
+            return null;
+        } else {
+            this.setState({
+                showToast: true,
+                toastTitle: "Error",
+                toastMessage: "You must select both a customer and a movie to create a rental." 
+            });
+        }
+       
     }
     render() {
+        const {showToast} = this.state;
+        const toggleShowToast = () => this.setState({ showToast: !showToast });
         return (
             <div className="selections">
+                <Toast show={showToast} onClose={toggleShowToast}>
+                    <Toast.Header>
+                        <strong className="mr-auto">{this.state.toastTitle}</strong>
+                        <small>just now</small>
+                    </Toast.Header>
+                    <Toast.Body>{this.state.toastMessage}</Toast.Body>
+                </Toast>
                 <Card style={{ width: '18rem' }}>
                 <Card.Header as="h5">Your Selections</Card.Header>
                 <Card.Body>
