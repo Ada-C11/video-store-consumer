667bd36b9ec56f4fbfb2c0bd53c33ac420485f95
amyesh <amyesh08@gmail.com>
2019-06-26 13:32:45 -0700

    cleaned up app file

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ class App extends Component {
     }
   }
 
- // Api Call to get movies from json
   componentDidMount() {
-      // Api call to get all customers
       axios.get('/customers')
       .then(response => {
         console.log(response)
@@ -32,17 +30,16 @@ class App extends Component {
       .catch(error => console.log(error))
   }
 
-  // Api call to get single movie
-  showMovieDetails = (title) => {
-    axios.get(`/movies/${title}`)
-      .then(response => {
-        console.log(response)
-        this.setState({
-          customerList: response.data
-        })
-      })
-      .catch(error => console.log(error))
-  }
+  // showMovieDetails = (title) => {
+  //   axios.get(`/movies/${title}`)
+  //     .then(response => {
+  //       console.log(response)
+  //       this.setState({
+  //         customerList: response.data
+  //       })
+  //     })
+  //     .catch(error => console.log(error))
+  // }
 
   addMovieToLibrary = (movie) => {
     const url = `http://localhost:3000/movies`;
@@ -50,42 +47,22 @@ class App extends Component {
     .then(() => {
       let success = `${movie.title} was successfully added to rental library!`;
       this.setState({userMessages: [success]})
-      // let movieLibraryUpdate = this.state.movieLibrary;
-      // this.setState({ movieLibrary: movieLibraryUpdate})
-      // alert(`${movie.title} was successfully added to rental library!`);
-      // console.log(response)
-      // this.setState({
-      //   movieLibrary: response.data
-      // })
     })
     .catch(error => console.log(error))
   }
 
   selectMovie = (movie) => {
     console.log(movie)
-    // return () => { 
       this.setState({
         selectedMovie: movie
       });
-    // }
-    // const movie = this.state.movieLibrary.find(movie => movie.id === movieId)
-    // console.log(this.state.selectedMovie)
   }
 
-  // this method takes in a name rather than an id
-  // we'll need the id to send to the checkout request
   selectCustomer = (customer) => {
     console.log(customer)
-    // return () => { 
       this.setState({
         selectedCustomer: customer
       });
-    // }
-    // const customer = this.state.customerLibrary.find(customer => customer.id === customerId)
-
-    // this.setState({
-    //   selectedCustomer: customer
-    // });
   }
 
   checkoutMovie = (movie, customer) => {
@@ -96,7 +73,8 @@ class App extends Component {
     };
     axios.post(`/rentals/${movie.title}/check-out`, checkoutParams)
     .then(() => {
-      alert(`${movie.title} was successfully checked out to ${customer.name}!`);
+      let success = `${movie.title} was successfully added to rental library!`;
+      this.setState({userMessages: [success]})
     })
     .catch(error => console.log(error));
   }
