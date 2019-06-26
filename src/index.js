efffd0aafd7356d05beb634e096b0af8640bd5da
Grace Shea <grace.m.shea@gmail.com>
2019-06-26 12:22:09 -0700

    navbar styling fixed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Library from './components/Library'
-import CustomerList from './components/CustomerList'
-import MovieSearch from './components/MovieSearch'
+
 
 
 import registerServiceWorker from './registerServiceWorker';
