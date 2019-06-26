7d71cc5ae67e1c44b8bca3611d14b80153c98824
Ariana Bray <aribray@gmail.com>
2019-06-26 10:38:05 -0700

    added rendering of search results with photo within search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import Movie from './Movie'
 
 require('dotenv').config();
 
@@ -40,20 +41,19 @@ class Search extends Component {
   };
 
   searchMovie = (searchTitle) => {
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchTitle}page=1&include_adult=false`
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchTitle}&page=1&include_adult=false`
 
     axios.get(URL)
     .then((response) => {
-      console.log(URL)
-      const movies = response.data.map((movie) => {
+      const movies = response.data.results.map((movie) => {
         return {
           id: movie.id,
           key: movie.id,
           title: movie.title,
           overview: movie.overview,
           release_date: movie.release_date,
-          image_url: movie.image_url,
-          external_id: movie.external_id
+          image_url: `http://image.tmdb.org/t/p/w185//${movie.poster_path}`,
+          external_id: movie.external_id,
         }
       })
       this.setState({
@@ -68,23 +68,41 @@ class Search extends Component {
   }
 
   render() {
+    const results = this.state.result.map((movie, i) => {
+
+      return <Movie
+            id={i}
+            key={i}
+            title={movie.title}
+            overview={movie.overview}
+            release_date={movie.release_date}
+            image_url={movie.image_url}
+            external_id={movie.external_id}
+      />
+    })
     return (
-      <form className="search-movie-form" onSubmit={this.onSearch}>
-        <div className="search-movie">
-            <h3 className="search-movie__header">Search Movie</h3>
-        </div>
-        <div>
-            <label 
-              className="search-movie-form__form-label" 
-              htmlFor="title">Movie Title</label>
-            <input className="search-movie-form__form-input"
-              name="searchTitle"
-              onChange={this.onInputChange}
-              value={this.state.searchTitle}>
-            </input>
-        </div>
-        <input className="search-movie-form__form-button" type="submit" name="submit" value="Search Movie" />
-    </form>
+      <div>
+        <form className="search-movie-form" onSubmit={this.onSearch}>
+          <div className="search-movie">
+              <h3 className="search-movie__header">Search Movie</h3>
+          </div>
+          <div>
+              <label 
+                className="search-movie-form__form-label" 
+                htmlFor="title">Movie Title</label>
+              <input className="search-movie-form__form-input"
+                name="searchTitle"
+                onChange={this.onInputChange}
+                value={this.state.searchTitle}>
+              </input>
+          </div>
+          
+          <input className="search-movie-form__form-button" type="submit" name="submit" value="Search Movie" />
+      </form>
+      <div>
+        {results}
+      </div>
+    </div>
     )};
   }
 
