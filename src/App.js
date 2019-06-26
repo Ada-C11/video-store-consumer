df83e0be0b53183c8ded54d1655b655a5f7738d4
Riyo <riyo.perry@gmail.com>
2019-06-26 12:33:03 -0700

    fix to app

--- src/App.js
+++ src/App.js
@@ -19,117 +19,118 @@ class App extends Component {
   }
 
   checkoutButtonClick = () => {
-    const title = this.state.currentMovie.title;
+    const movie = this.state.currentMovie;
     const customer = this.state.currentCustomer;
-    const rentalUrl = "http://localhost:3000/rentals" + title + "/check-out";
+    const rentalUrl = "http://localhost:3000/rentals/" + movie.title + "/check-out";
 
 
-    if (this.state.currentCustomer === "" || this.state.currentMovie === "") {
+    if (customer === "" || movie === "") {
       return (<p>Please select a movie AND a customer.</p>)
     } else {
 
       axios.post(rentalUrl)
         .then((response) => {
           console.log(response)
-          this.state.currentCustomer.movies_checked_out_count += 1;
+          customer.movies_checked_out_count += 1;
         })
         .catch((error) => {
           this.setState({ errorMessages: error.message });
           console.log(this.state.errorMessages)
         });
-      }
     }
+  }
 
 
-      currentCustomerCallback = (customer) => {
+  currentCustomerCallback = (customer) => {
+    return () => {
+      this.setState({
+        currentCustomer: customer,
+      })
+    }
+  }
 
-        return () => {
-          this.setState({
-            currentCustomer: customer,
-          })
-        }
-      }
+  currentMovieCallback = (movie) => {
+    return () => {
+      this.setState({
+        currentMovie: movie,
+      })
+    }
+  }
 
-      currentMovieCallback = (movie) => {
+  render() {
+    return (
+      <section>
 
-        return () => {
-          this.setState({
-            currentMovie: movie,
-          })
-        }
-      }
+        <Router>
+          <div>
+            <Header />
 
-      render() {
-        return (
-          <section>
+            <div className="current_selections">
+              <p><strong>Selected Customer:</strong>{this.state.currentCustomer.name}</p>
+              <p><strong>Selected Movie:</strong> {this.state.currentMovie.title}</p>
 
-            <Router>
-              <div>
-                <Header />
-
-                <div className="current_selections">
-                  <p><strong>Selected Customer:</strong>{this.state.currentCustomer.name}</p>
-                  <p><strong>Selected Movie:</strong> {this.state.currentMovie.title}</p>
-
-                  <button className="checkout_button"
+              <button className="checkout_button"
                 onClick={this.checkoutButtonClick}>Checkout</button>
-                </div>
+            </div>
 
-                <Route exact path="/" component={Home} />
-                <Route
-                  path="/customers"
-                  render={(routeProps) => (
-                    <Customer {...routeProps}
-                      currentCustomerCallback={this.currentCustomerCallback} />
-                  )}
-                />
-                <Route
-                  path="/library"
-                  render={(routeProps) => (
-                    <Movie {...routeProps}
-                      currentMovieCallback={this.currentMovieCallback} />
-                  )} />
-                <Route path="/search" component={Search} />
+            <Route exact path="/" component={Home} />
+            <Route
+              path="/customers"
+              render={(routeProps) => (
+                <Customer {...routeProps}
+                  currentCustomerCallback={this.currentCustomerCallback} />
+              )}
+            />
+            <Route
+              path="/library"
+              render={(routeProps) => (
+                <Movie {...routeProps}
+                  currentMovieCallback={this.currentMovieCallback} />
+              )} />
+            <Route path="/search"
+              render={(routeProps) => (
+                <Search {...routeProps} />
+              )} />
 
-              </div>
+          </div>
 
-            </Router>
-
-
-          </section>
-        );
-      }
-    }
+        </Router>
 
 
-    function Home() {
-      return <VideoStore
-      />;
-    }
+      </section>
+    );
+  }
+}
 
-    // function MovieSearch() {
-    //   return <Search />;
-    // }
 
-    function Header() {
-      return (
-        <ul>
-          <li>
-            <Link to="/">VideoStore</Link>
-          </li>
-          <li>
-            <Link to="/customers">Customers</Link>
-          </li>
-          <li>
-            <Link to="/library">Library</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-        </ul>
+function Home() {
+  return <VideoStore
+  />;
+}
 
-      );
+// function MovieSearch() {
+//   return <Search />;
+// }
 
-    }
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">VideoStore</Link>
+      </li>
+      <li>
+        <Link to="/customers">Customers</Link>
+      </li>
+      <li>
+        <Link to="/library">Library</Link>
+      </li>
+      <li>
+        <Link to="/search">Search</Link>
+      </li>
+    </ul>
 
-    export default App;
+  );
+
+}
+
+export default App;
