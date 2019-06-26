4a727b4f48d5cfc971ace96eca0a72485b5bac62
Shubha Rajan <shubharajan94@gmail.com>
2019-06-26 14:03:00 -0700

    created separate component for notifications

--- src/components/Selected.js
+++ src/components/Selected.js
@@ -3,20 +3,12 @@ import PropTypes from 'prop-types';
 import {Card, Button, Toast} from 'react-bootstrap'
 import axios from 'axios';
 import moment from 'moment';
-import Moment from 'react-moment';
+
+import Notification from "./Notification"
 
 import './Selected.css'
 class Selected extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showToast: false,
-            toastTitle: '',
-            toastMessage: '',
-            toastTimestamp:'',
-            error:true
-        }
-    }
+ 
     onReserveButtonClick = () =>{
         if (this.props.movie && this.props.customer) {
             const endpoint = `http://localhost:3000/rentals/${this.props.movie.title}/check-out`;
@@ -26,48 +18,42 @@ class Selected extends Component {
             }
             axios.post(endpoint, params)
                 .then((response)=>{
-                    console.log(response);
-                    this.setState({
-                        showToast: true,
-                        toastTitle: "Success!",
-                        toastMessage: `Successfully rented ${this.props.movie.title} to ${this.props.customer.name}. Your movie is due on ${response.data.due_date} `,
-                        toastTimestamp: Date.now(),
-                        error: false
-                    });
+                    this.props.addNotificationCallback(
+                        < Notification
+                        toastTitle= {"Success!"}
+                        toastMessage= {`Successfully rented ${this.props.movie.title} to ${this.props.customer.name}. Your movie is due on ${response.data.due_date}. `}
+                        toastTimestamp= {Date.now()}
+                        error= {false}
+                        />
+                    );
                 })
                 .catch((error)=>{
-                    this.setState({
-                        showToast: true,
-                        toastTitle: "Error",
-                        toastMessage: `Could not create rental: ${error.message}`,
-                        toastTimestamp: Date.now()
-                    });
+                    this.props.addNotificationCallback (
+                        <Notification
+                        toastTitle= {"Error!"}
+                        toastMessage= {`Could not create rental: ${error.message}`}
+                        toastTimestamp= {Date.now()}
+                        error= {true}
+                        />
+                    );
+                    ;
                 })
         } else {
-            this.setState({
-                showToast: true,
-                toastTitle: "Error",
-                toastMessage: "You must select both a customer and a movie to create a rental." ,
-                toastTimestamp: Date.now()
-            });
+                    this.props.addNotificationCallback (
+                        <Notification
+                        toastTitle= {"Error!"}
+                        toastMessage= {"You must select both a customer and a movie to create a rental."}
+                        toastTimestamp= {Date.now()}
+                        error= {true}
+                        />
+                    );
         }
        
     }
     render() {
-        const {error, showToast, toastTitle, toastMessage, toastTimestamp} = this.state;
-        const toggleShowToast = () => this.setState({ showToast: !showToast });
+    
         return (
             <div className="selections">
-                <Toast className={error ? 'error' : 'success'}show={showToast} onClose={toggleShowToast}>
-                    <Toast.Header>
-                        <strong className="mr-auto">{toastTitle}</strong>
-                        <small>
-                            <Moment fromNow>{toastTimestamp}</Moment>
-                        </small>
-                    </Toast.Header>
-                    <Toast.Body>{toastMessage}</Toast.Body>
-                </Toast>
-
                 <Card style={{ width: '18rem' }}>
                 <Card.Header as="h5">Your Selections</Card.Header>
                 <Card.Body>
