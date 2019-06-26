d023080c1615f208787b23776e215b0d2a16efa3
rosalyn <rosalyn.anuseh@gmail.com>
2019-06-26 11:52:24 -0700

    Added more details to customer list

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -21,40 +21,48 @@ class CustomerList extends Component {
     this.props.rentMovieWithCustomerCallback(customer);
   };
   componentDidMount() {
-    axios.get(`http://localhost:3001/customers`)
-      .then((response) => {
-        const customers = response.data.map((customer) => {
-
-       return(
-         <p key= {customer.id}>{customer.name}</p>
-       )
-        })
-        console.log(response.data)
+    axios
+      .get(`http://localhost:3000/customers`)
+      .then(response => {
+        const customers = response.data.map(customer => {
+          return customer;
+        });
+        console.log(response.data);
         this.setState({
-          customers: customers,
+          customers: customers
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
-        alert('Error happened');
+        alert("An error occurred");
         this.setState({ error: error.message });
       });
   }
 
   render() {
-    const customers = this.state.customers.map(customer => {
+    const customers = this.state.customers.map((customer, i) => {
       return (
-        <section key={customer}>
-          <span> {customer} </span>
+        <section key={i}>
+          <div className="card w-75">
+            <div className="card-body">
+              <h5 className="card-title">Name: {customer.name}</h5>
+              <p className="card-text">ID: {customer.id}</p>
+              <p className="card-text">
+                # of Movies checked out: {customer.movies_checked_out_count}
+              </p>
+              <p className="card-text">Phone number: {customer.phone}</p>
 
-          <button
-            type="button"
-            className="btn btn-danger"
-            aria-label="Close"
-            onClick={() => this.selectToCheckoutCustomer(customer)}
-          >
-            SELECT
-          </button>
+              <br />
+              <button
+                type="button"
+                className="btn btn-danger"
+                aria-label="Close"
+                onClick={() => this.selectToCheckoutCustomer(customer)}
+              >
+                SELECT
+              </button>
+            </div>
+          </div>
         </section>
       );
     });
