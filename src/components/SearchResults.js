89ef180af06a6cd167bdf6c95dd078cf98715c23
Jansen <v.jansen.martin@gmail.com>
2019-06-26 17:11:35 -0700

    Remove comments from selectMovie function

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,31 +6,8 @@ import Message from './Message.js'
 const SearchResults = (props) => {
 
     const selectMovie = (event) => {
-
-        
-        const movie = props.movieData[event.target.id]
-
-        props.addMovieCallback(movie);
-        
-        // const movie = props.movieData[event.target.id]
-        // console.log(movie);
-        // const postURL = 'http://localhost:3002/movies/add-movie'
-        // axios.post(postURL, null, {
-        //     params: {
-        //         external_id: movie.external_id,
-        //         image_url: movie.image_url,
-        //         overview: movie.overview,
-        //         release_date: movie.release_date,
-        //         title: movie.title
-        //     }
-        // })
-        // .then((response) => {
-        //     console.log(response)
-
-        // })
-        // .catch((error) => {
-        //     console.log(error.message)
-        // })
+      const movie = props.movieData[event.target.id]
+      props.addMovieCallback(movie);
     }
 
     const movies = props.movieData.map((movie, i) => {
