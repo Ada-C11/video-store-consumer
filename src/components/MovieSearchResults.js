36c16055b0e89b4db4d224ddb42b65e26975d930
Bitaman <b.amani.s@gmail.com>
2019-06-27 13:56:35 -0700

    added axios call to add a movie to the library

--- src/components/MovieSearchResults.js
+++ src/components/MovieSearchResults.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Movie from "./Movie";
 import "./MovieSearchResults.css";
+import axios from "axios";
 
 class MovieSearchResults extends Component {
   constructor(props) {
@@ -17,6 +18,16 @@ class MovieSearchResults extends Component {
     });
 
     this.props.addMovietoLibrayCallback(movie);
+    axios.post(`http://localhost:3001/movies`, { title: movie.title, overview: movie.overview, release_date: movie.release_date, inventory: 5})
+    .then((response) => {
+      return response.data
+    })
+
+    .catch(error => {
+      console.log(error.messages);
+      alert("Error happened");
+      this.setState({ error: error.message });
+    });
   };
 
   render() {
