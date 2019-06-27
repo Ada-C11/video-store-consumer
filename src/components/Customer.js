1be56db36a944d12ca484d3fd8a76c085483ccc9
Ariana Bray <aribray@gmail.com>
2019-06-27 13:44:57 -0700

    added styling for CustomerList and Customer components

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Customer.css'
 
 const Customer = (props) => {
   const selectCustomer = () => {
@@ -7,17 +8,20 @@ const Customer = (props) => {
   }
 
   return (
-    <div className="customer">
-        <div className="content">
-          <ul>{props.name}</ul>
-            <li>{props.address}</li>
-            <li>{props.city}, {props.state}</li>
-            <div>{props.postal}</div>
-
-            <li>{props.phone}</li>
-            <li>${props.account_credit} account credit</li>
-            <li><a href="#" onClick={selectCustomer}>Select Customer</a></li>
-        </div>
+    <div>
+        <div className="customer card">
+            <div className="content card-body">
+              <ul className="customer__info">
+                <li className="card-title">{props.name}</li>
+                  <li className="card-text">{props.address}</li>
+                  <li className="card-text">{props.city}, {props.state}</li>
+                  <li className="card-text money">${props.account_credit} account credit</li>
+                  </ul>
+                  <div className="select__button">
+                    <button className="select__customer btn btn-dark"><a href="#" onClick={selectCustomer}>Select Customer</a></button>
+                  </div>
+            </div>
+          </div>
     </div>
 
   )
