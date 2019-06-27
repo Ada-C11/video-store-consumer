2179fb30431a33fb480e879c8cf012e0b08edf71
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-06-27 08:56:23 -0700

    added awesome videotape image

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 
 const Index = () => {
-  return <h2>Clever Video Store Name</h2>;
+  return (
+    <div>
+  <h2>Clever Video Store Name</h2>
+  <img src="https://sdav.com/wp-content/uploads/2015/05/videotapes.jpg" />
+  </div>
+  );
 }
 
 export default Index
