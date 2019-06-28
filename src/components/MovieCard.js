1efa5ca2f108a5614daee72d99c21dc99eb7c5b0
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-28 10:37:18 -0700

    all button colors ammended

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
+import './MovieCard.css';
 
 const MovieCard = (props) => {
     const {image_url, title, overview, release_date,addMovieCallback } = props;
@@ -15,7 +16,7 @@ const MovieCard = (props) => {
                 <h3 className="title">{title}</h3>
                 <p><span className="bold_words">Release Date: </span>{release_date}</p>
                 <p><span className="bold_words">Overview: </span>{overview}</p>
-                <Button onClick={addMovie}>Add to Library</Button>
+                <Button variant="dark" id="button_font" onClick={addMovie}>Add to Library</Button>
             </div>
             </td>
         </tr>
